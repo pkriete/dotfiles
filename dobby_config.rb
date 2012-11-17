51e3c4024c456d8d929f0f479988dfc263021341
Pascal Kriete <pascal@pascalkriete.com>
2012-11-17 01:03:50 -0500

    Cleaning up dobby_config

--- dobby_config.rb
+++ dobby_config.rb
@@ -1,218 +1,129 @@
 
 # Standard Processes
 
-service :mysql do |s|
-  s.name  = "MySQL"
-  s.file  = "/usr/local/my.cnf"
+service :apache do |s|
+  s.name = 'Apache'
+  s.file = '/private/etc/apache2/httpd.conf'
 
-  s.start = "mysql.server start"
-  s.stop  = "mysql.server stop"
+  s.start   = 'apachectl start'
+  s.stop    = 'apachectl stop'
+  s.restart = 'apachectl restart'
 
-  s.process = "mysqld"
-end
-
-service :memcached do |s|
-  s.name  = "Memcached"
-  s.start = "/usr/local/bin/memcached" # -d for daemon
-  s.process  = "memcached"
-end
-
-service :php => :apache do |s|
-  s.name  = "PHP"
-  s.file  = "/usr/local/etc/php/5.4/php.ini"
-
-  s.start = "php -i"
-  s.process = "phpserver"
-
-  s.start = Proc.new do |args|
-    cmd = "php -i"
-    cmd << "-p#{args.port}" if args.port
-    cmd << "-f#{args.file}" if args.file
-  end
+  s.process = 'httpd'
+  s.needs_root = true
 end
 
 service :jekyll do |s|
-  s.name = "Jekyll"
-  s.start = "jekyll --auto --server"
+  s.name = 'Jekyll'
+  s.start = 'jekyll --auto --server'
   s.process = 'jekyll'
 end
 
-service :apache do |s|
-  s.name = "Apache"
-  s.file = "private/etc/apache2/httpd.conf"
+service :memcached do |s|
+  s.name  = 'Memcached'
+  s.start = '/usr/local/bin/memcached' # -d for daemon
+  s.process  = 'memcached'
+end
 
-  s.start   = "apachectl start"
-  s.stop    = "apachectl stop"
-  s.restart = "apachectl restart"
+service :mysql do |s|
+  s.name  = 'MySQL'
+  s.file  = '/usr/local/my.cnf'
 
-  s.process = "httpd"
-  s.needs_root = true
+  s.start = 'mysql.server start'
+  s.stop  = 'mysql.server stop'
+
+  s.process = 'mysqld'
+end
+
+service :php => :apache do |s|
+  s.name  = 'PHP'
+  s.file  = '/usr/local/etc/php/5.4/php.ini'
+
+  s.process = 'php'
+  s.start = Proc.new do |args|
+
+    flags = args.parse({
+      '-p' => { :default => 8000, :alias => '--port' },
+      '-t' => { :alias => '--root' }
+    })
+
+    cmd = "php -S localhost:#{flags['-p']}"
+    cmd << " -t #{flags['-t']}" if flags['-t']
+    cmd
+  end
+end
+
+service :postgres do |s|
+  s.name = 'Postgres'
+  # s.file = ''
+
+  s.start = 'pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+  s.stop = 'pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+
+  s.process = 'postgres'
+end
+
+service :redis do |s|
+  s.name = 'Redis'
+  s.file = '/usr/local/etc/redis.conf'
+
+  s.start = 'redis-server /usr/local/etc/redis.conf'
+  s.process = 'redis-server'
 end
 
 # Other Config Files
 
-config :apc => :php do |c|
-  c.name = "APC"
-end
-
 config :bash do |c|
-  c.name = "Bash Profile"
-  c.file = "~/.bash_profile"
+  c.name = 'Bash Profile'
+  c.file = '~/.bash_profile'
 end
 
 config :dobby do |c|
-  c.name = "Dobby Config"
-  c.file = "~/.dobby_config"
+  c.name = 'Dobby Config'
+  c.file = '~/.dobby_config'
 end
 
 config :git do |c|
-  c.name = "Gitconfig"
-  c.file = "~/.gitconfig"
+  c.name = 'Gitconfig'
+  c.file = '~/.gitconfig'
 end
 
 config :gitignore do |c|
-  c.name = "Gitignore"
-  c.file = "~/.gitignore"
+  c.name = 'Gitignore'
+  c.file = '~/.gitignore'
 end
 
 config :hosts do |c|
-  c.name = "Hosts File"
-  c.file = "/etc/hosts"
+  c.name = 'Hosts File'
+  c.file = '/etc/hosts'
+end
+
+config :php_apc => :php do |c|
+  c.name = 'APC (PHP)'
+  c.file = '/usr/local/etc/php/5.4/conf.d/ext-apc.ini'
+end
+
+config :php_memcached => :php do |c|
+  c.name = 'Memcached (PHP)'
+  c.file = '/usr/local/etc/php/5.4/conf.d/ext-memcached.ini'
+end
+
+config :php_mongo => :php do |c|
+  c.name = 'Mongo (PHP)'
+  c.file = '/usr/local/etc/php/5.4/conf.d/ext-mongo.ini'
+end
+
+config :php_redis => :php do |c|
+  c.name = 'Redis (PHP)'
+  c.file = '/usr/local/etc/php/5.4/conf.d/ext-redis.ini'
+end
+
+config :php_xdebug => :php do |c|
+  c.name = 'Xdebug (PHP)'
+  c.file = '/usr/local/etc/php/5.4/conf.d/ext-xdebug.ini'
 end
 
 config :vhosts => :apache do |c|
-  c.name = "Virtual Hosts"
-  c.file = "/private/etc/apache2/extra/httpd-vhosts.conf"
+  c.name = 'Virtual Hosts'
+  c.file = '/private/etc/apache2/extra/httpd-vhosts.conf'
 end
-
-
-# Scripted Automatic Edits
-# Pipedream work in progress
-
-class ScriptedTask
-
-  def self.service symbol
-    # grab config file name?
-  end
-
-  def self.config symbol
-    # grab config file name?
-  end
-
-end
-
-
-
-class Vhosts < ScriptedTask
-
-  config :vhosts
-
-  def list
-    # show all files
-    # merge with all in vhosts config
-  end
-
-  def create name, args
-    # check if folder exists in vhosts
-    # create folder in vhosts
-    # create "public folder"
-    # add to vhosts config
-    # add to hosts file
-  end
-
-  def remove name, args
-    # read file
-    # check for dir
-    # check for -f flag for forced removal
-  end
-
-  def rename name, new_name
-    # check for dir
-    # grep though file
-    # rename dir
-    # update file
-  end
-
-end
-
-class Github < ScriptedTask
-
-  def list
-    # list directories
-  end
-
-  def add path, name = nil
-    name ||= Pathname.basename(path)
-
-    # check for dir
-    # basepath path if name not given
-    # symlink
-    # check for .git -> git init
-    # check for git remote -> add github?
-  end
-
-  private
-    def symlink
-    end
-
-    def create_git
-    end
-
-end
-
-
-# class AddVhost < ConfiggerAuto
-
-#   def edit_vhost file, name
-
-#   end
-
-#   def edit_hosts file
-#     # check if file contains line
-#     file.append("127.0.0.1  name   # /vhosts/name")
-#   end
-
-# end
-
-
-# class GithubProject < ConfiggerAuto
-
-#   def add
-#   end
-
-#   private
-#     def symlink
-#     end
-
-#     def create_git
-#     end
-
-# end
-
-# class ToggleAPC < ConfiggerAuto
-
-#   @options = ["on", "off"]
-#   @description = "Turn APC on or off"
-
-#   attr_reader :options, :description
-
-#   def current?
-#     "On"
-#   end
-
-# end
-
-# class PHPVersion < ConfiggerAuto
-
-#   @options = ["PHP 5.3", "PHP 5.4"]
-#   @description = "Change PHP Version"
-
-#   attr_reader :options, :description
-
-#   def current?
-#     "PHP 5.3" # should return current version
-#   end
-
-# end
-
-# automate :php, PHPVersion.new
